eb5caa75e96374371ae798708546790d77c1ea08
Alex Roy <Alex@AlexMBP.local>
2018-10-26 17:20:40 -0400

    Added 'provider average' performance tracking pt. 2
    now logs provider average RMSE in txt file

--- Post_mortem.py
+++ Post_mortem.py
@@ -11,8 +11,8 @@ loggr = logging.getLogger(__name__)
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setFormatter(
     logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line " +
-        "%(lineno)d"
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line "
+        + "%(lineno)d"
     )
 )
 log_handler.setLevel(logging.INFO)
@@ -49,25 +49,33 @@ def post_mortem(**kwargs):
         comp = pred.merge(actual, on=["region", "province"], how="left")
     except KeyError:
         comp = pred.merge(actual, on=["region"], how="left")
-    comp["pv_ave"] = (comp["TWN_high_T1"] + comp["EC_high_T1"]) / 2
+    comp["TWN_EC_ave"] = (comp["TWN_high_T1"] + comp["EC_high_T1"]) / 2
     comp["diff_real"] = (comp["high"]) - comp["model_predictions"]
     comp["diff_TWN_rival"] = (comp["high"]) - comp["TWN_high_T1"]
     comp["diff_EC_rival"] = (comp["high"]) - comp["EC_high_T1"]
-    #comp = comp.sort_values("diff_real")
+    comp["diff_mean_rival"] = (comp["high"]) - comp["TWN_EC_ave"]
+    # comp = comp.sort_values("diff_real")
     ML_perf = (
-        (comp["diff_real"].apply(lambda x: x ** 2).sum()) / len(comp.index)) ** 0.5
+        (comp["diff_real"].apply(lambda x: x ** 2).sum()) / len(comp.index)
+    ) ** 0.5
     TWN_rival_perf = (
-        (comp["diff_TWN_rival"].apply(lambda x: x ** 2).sum()) / len(comp.index)) ** 0.5
+        (comp["diff_TWN_rival"].apply(lambda x: x ** 2).sum()) / len(comp.index)
+    ) ** 0.5
     EC_rival_perf = (
-        (comp["diff_EC_rival"].apply(lambda x: x ** 2).sum()) / len(comp.index)) ** 0.5
+        (comp["diff_EC_rival"].apply(lambda x: x ** 2).sum()) / len(comp.index)
+    ) ** 0.5
+    mean_rival_perf = (
+        (comp["diff_mean_rival"].apply(lambda x: x ** 2).sum()) / len(comp.index)
+    ) ** 0.5
     comp.to_csv(
         "{}/Predictions/{}{}_compare_actual.csv".format(
             PATH, time_travel_string, actual_date
         )
     )
     loggr.info("ML performance: {}".format(ML_perf))
-    loggr.info("TWN Rival performance: {}".format(TWN_rival_perf))
-    loggr.info("EC Rival performance: {}".format(EC_rival_perf))
+    loggr.info("TWN rival performance: {}".format(TWN_rival_perf))
+    loggr.info("EC rival performance: {}".format(EC_rival_perf))
+    loggr.info("Mean rival performance: {}".format(mean_rival_perf))
     f = open(
         "{}/Predictions/{}{}_compare_actual.txt".format(
             PATH, time_travel_string, actual_date
@@ -75,5 +83,6 @@ def post_mortem(**kwargs):
         "w",
     )
     f.write("ML performance: {}\n".format(ML_perf))
-    f.write("TWN Rival performance: {}\n".format(TWN_rival_perf))
-    f.write("EC Rival performance: {}".format(EC_rival_perf))
+    f.write("TWN rival performance: {}\n".format(TWN_rival_perf))
+    f.write("EC rival performance: {}".format(EC_rival_perf))
+    f.write("Mean rival performance: {}".format(mean_rival_perf))

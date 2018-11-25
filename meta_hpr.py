2189180443652a717decc84385b91dd542da8653
Alex Roy <Alex@AlexMBP.local>
2018-11-24 22:30:15 -0500

    Tweaked for a Better Report

--- meta_hpr.py
+++ meta_hpr.py
@@ -5,14 +5,16 @@ def analyze():
 
 	hpr['date'] = hpr.log_time.apply(lambda x: x[:10])
 	hpr['goal'] = (hpr.ML_rms - hpr.Mean_rms) / hpr.Mean_rms
+	hpr['goal_TWN'] = (hpr.ML_rms - hpr.TWN_rms) / hpr.TWN_rms
+	hpr['goal_EC'] = (hpr.ML_rms - hpr.EC_rms) / hpr.EC_rms
 
 	starting_index = hpr[(hpr.date=="2018-11-19")&(hpr.max_features != 'auto')].index[0]
 
 	hpr = (hpr[hpr.index>starting_index])
 	hpr['max_features'] = hpr.max_features.apply(float)
 
-	feat_analysis = hpr.corr().goal.apply(abs).drop(list(hpr.filter(regex="_rms"))).drop(list(hpr.filter(regex="_ave"))).drop(list(hpr.filter(regex="goal"))).dropna().sort_values(ascending=False)
+	feat_analysis = hpr.corr().goal.apply(abs).drop(list(hpr.filter(regex="_rms"))).drop(list(hpr.filter(regex="_ave"))).drop('goal').dropna().sort_values(ascending=False)
 
-	meta_hpr = hpr.sort_values('goal')[['log_time']+['goal']+list(hpr.filter(feat_analysis.index))]
+	meta_hpr = hpr[['log_time']+['goal']+list(hpr.filter(feat_analysis.index))]
 
 	meta_hpr.to_csv('/Users/Alex/Dropbox (Personal)/HPMetaResults.csv')

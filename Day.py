34d4728735848c42e93cebe19ee76b13143f0980
Alex Roy <Alex@AlexMBP.local>
2018-11-28 21:29:23 -0500

    Complete Overhaul of Daay.py and Evening.py
    Brought them up to speed with the rest of the code base. They hadn't been used in a while and were completely neglected.

--- Day.py
+++ Day.py
@@ -5,13 +5,17 @@ import os
 import datetime
 import time
 from shutil import copyfile
+import random
+import pandas as pd
+import json
 
+from Test_data import check_historical_data
+from Test_data import correct_bad_EC_data
 from Wrangle import wrangle
 from Train import train
 from Post_mortem import post_mortem
+from meta_hpr import analyze
 
-# Hyperparameters
-time_span = 10
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,11 +23,32 @@ loggr = logging.getLogger(__name__)
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setFormatter(
     logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line "
-        + "%(lineno)d"
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line %(lineno)d"
     )
 )
 loggr.addHandler(log_handler)
+loggr.setLevel(logging.INFO)
+
+def load_hyperparameters():
+    filename = "/Users/Alex/Dropbox (Personal)/hyperparameters.json"
+    try:
+        with open(filename, "rb") as input_file:
+            return json.load(input_file)
+    except FileNotFoundError:
+        return {}
+
+def get_datetime(date):
+    return datetime.date(int(date[:4]), int(date[5:7]), int(date[8:]))
+
+hp = load_hyperparameters()
+hp.update({
+    "iterations": 1,
+    "start_date": str(datetime.datetime.now().date()),
+    "end_date": str(datetime.datetime.now().date()),
+    "edge_forecasting": 1,
+    "real_time": 1,
+    "exit_on_exception": 1
+})
 
 loggr.info("Starting ETL process...")
 
@@ -49,39 +74,84 @@ time.sleep(60)
 
 try:
     loggr.info("Running a few tests...")
-    from Test_data import check_historical_data
-    from Test_data import correct_bad_EC_data
-
     check_historical_data()
     correct_bad_EC_data()
     loggr.info("Tests are now complete.")
 except Exception as e:
     loggr.exception("Test_data.py could not run. Here's why: \n {e}")
+
 try:
-    loggr.info("Wrangling data to prep for ML " "Analysis...")
-    wrangle()
-    loggr.info("'master_db.csv' is now updated with the latest weather!")
-    copyfile(
-        "{}/Data/master_db.csv".format(PATH),
-        "/Users/Alex/Dropbox (Personal)/master_db.csv",
-    )
+    loggr.info("Wrangling data before training.")
+    wrangle_status = wrangle(
+        target_date=hp['start_date'],
+        time_span=hp["time_span"],
+        rolling_average_window=hp["rolling_average_window"],
+        rolling_average_min_periods=hp[
+            "rolling_average_min_periods"
+        ],
+        TWN_EC_split = hp["TWN_EC_split"],
+        date_efficient=hp["date_efficient"],
+        region_efficient=hp["region_efficient"],
+        drop_columns=hp['drop_columns'],
+        include_only_columns=hp['include_only_columns'],
+        label=hp["label"],
+        real_time=hp["real_time"],
+        )
+    loggr.info("Wrangling Complete")
 except Exception as e:
     loggr.exception("Wrangle.py could not run. Here's why: \n {e}")
-loggr.info("ETL process is now complete.")
+
 try:
-    loggr.info("Crunching the numbers with ML...")
-    train(time_span=time_span)
-    loggr.info("ML complete. Pickled model is ready!")
-except Exception as e:
-    loggr.exception("ML.py could not run. Here's why: \n {e}")
-try:
-    loggr.info(
-        "Comparing predictions with real-world results for date of "
-        "{}".format(datetime.datetime.now().date() - datetime.timedelta(days=1))
+    loggr.info("Training on wrangled data to produce a model for evening predictions".format(datetime.datetime.now().date() + datetime.timedelta(1)))
+    train(
+        target_date=hp['start_date'],
+        label=hp["label"],
+        time_span=hp["time_span"],
+        max_depth=hp["max_depth"],
+        max_features=hp["max_features"],
+        min_samples_leaf=hp["min_samples_leaf"],
+        min_samples_split=hp["min_samples_split"],
+        n_estimators=hp["n_estimators"],
+        cv=hp["cv"],
+        precision=hp["precision"],
+        edge_forecasting=hp["edge_forecasting"],
+        normalize_data=hp["normalize_data"],
     )
-    post_mortem()
-    loggr.info("Post_mortem results are ready!")
-except FileNotFoundError:
-    loggr.critical("Missing data - cannot compare performances " "for this date")
+    loggr.info("ML Model is now ready")
+except Exception as e:
+    loggr.exception("Train.py could not run. Here's why: \n {e}")
+
+###Post_mortem (Dates will be tricky here!)
+try:
+    loggr.info("Preparing predictions table for tomorrow ({})".format(datetime.datetime.now().date() + datetime.timedelta(1)))
+    ML, TWN, EC, Mean = post_mortem(target_date=str(get_datetime(hp['start_date'])-datetime.timedelta(1)))
+    loggr.info("Post_mortem results for yesterday ({}) are now ready".format(datetime.datetime.now().date() - datetime.timedelta(1)))
 except Exception as e:
     loggr.exception("Post_mortem.py could not run. Here's why: \n {e}")
+
+hp.update(
+        {
+            "log_time": datetime.datetime.now(),
+            "1ML_rms": ML,
+            "1TWN_rms": TWN,
+            "1EC_rms": EC,
+            "1Mean_rms": Mean,
+        }
+    )
+
+loggr.info("Saving results to `HPresults.csv`")
+try:
+    search_results = pd.read_csv(
+        "/Users/Alex/Dropbox (Personal)/HPResults.csv"
+    )
+except FileNotFoundError:
+    search_results = pd.DataFrame(columns=(["log_time"] + list(hp.keys())))
+search_results = search_results.append(hp_inst, ignore_index=True)
+try:
+    search_results.to_csv(
+        "/Users/Alex/Dropbox (Personal)/HPResults.csv", index=False
+    )
+except FileNotFoundError:
+    loggr.warning("Could not save results!!!")
+
+analyze()
--- Evening.py
+++ Evening.py
@@ -2,10 +2,15 @@ import logging
 import requests
 import sys
 import os
+import datetime
 import time
 from shutil import copyfile
+import random
+import pandas as pd
+import json
 
 from Test_data import check_forecast_data
+from Wrangle import wrangle
 from Predict import predict
 
 
@@ -15,11 +20,32 @@ loggr = logging.getLogger(__name__)
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setFormatter(
     logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line "
-        + "%(lineno)d"
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s - line %(lineno)d"
     )
 )
 loggr.addHandler(log_handler)
+loggr.setLevel(logging.INFO)
+
+def load_hyperparameters():
+    filename = "/Users/Alex/Dropbox (Personal)/hyperparameters.json"
+    try:
+        with open(filename, "rb") as input_file:
+            return json.load(input_file)
+    except FileNotFoundError:
+        return {}
+
+def get_datetime(date):
+    return datetime.date(int(date[:4]), int(date[5:7]), int(date[8:]))
+
+hp = load_hyperparameters()
+hp.update({
+    "iterations": 1,
+    "start_date": str(datetime.datetime.now().date()),
+    "end_date": str(datetime.datetime.now().date()),
+    "edge_forecasting": 1,
+    "real_time": 1,
+    "exit_on_exception": 1
+})
 
 loggr.info("Starting ETL process...")
 try:
@@ -55,9 +81,61 @@ try:
     loggr.info("Tests are now complete.")
 except Exception as e:
     loggr.exception("Test_data.py could not run. Here's why: \n {e}")
+
 try:
-    loggr.info("Predicting forecast using today pickled model from today...")
-    predict()
-    loggr.info("ML forecast is ready!")
+    loggr.info("Preparing predictions table for tomorrow ({})".format(datetime.datetime.now().date()+datetime.timedelta(1)))
+    wrangle_status = wrangle(
+        target_date=hp['start_date'],
+        time_span=hp["time_span"],
+        rolling_average_window=hp["rolling_average_window"],
+        rolling_average_min_periods=hp[
+            "rolling_average_min_periods"
+        ],
+        TWN_EC_split = hp["TWN_EC_split"],
+        date_efficient=hp["date_efficient"],
+        region_efficient=hp["region_efficient"],
+        drop_columns=hp['drop_columns'],
+        include_only_columns=hp['include_only_columns'],
+        label=hp["label"],
+        real_time=hp["real_time"],
+        )
+    loggr.info("Predictions table is now ready")
+except Exception as e:
+    loggr.exception("Wrangle.py could not run. Here's why: \n {e}")
+
+try:
+    loggr.info("Running predictions for tomorrow ({})".format(datetime.datetime.now().date() + datetime.timedelta(1)))
+    ML, TWN, EC, Mean = predict(
+        label=hp["label"],
+        precision=hp["precision"],
+        target_date=hp['start_date'],
+        normalize_data=hp["normalize_data"],
+    )
+    hp.update(
+        {
+            "log_time": datetime.datetime.now(),
+            "1ML_rms": ML,
+            "1TWN_rms": TWN,
+            "1EC_rms": EC,
+            "1Mean_rms": Mean,
+        }
+    )
+
+    loggr.info("Predictions are now ready")
 except Exception as e:
     loggr.exception("Predict.py could not run. Here's why: \n {e}")
+
+loggr.info("Saving results to `HPresults.csv`")
+try:
+    search_results = pd.read_csv(
+        "/Users/Alex/Dropbox (Personal)/HPResults.csv"
+    )
+except FileNotFoundError:
+    search_results = pd.DataFrame(columns=(["log_time"] + list(hp.keys())))
+search_results = search_results.append(hp_inst, ignore_index=True)
+try:
+    search_results.to_csv(
+        "/Users/Alex/Dropbox (Personal)/HPResults.csv", index=False
+    )
+except FileNotFoundError:
+    loggr.warning("Could not save results!!!")

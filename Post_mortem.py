2907e6c3462d5e6474b45bd86cd7868465f73715
Alex Roy <Alex@AlexMBP.local>
2018-12-05 22:10:48 -0500

    Now saving separate csv for just yesterday's results
    added logic as well to skip logging for yesteday results if there was nothing reported

--- Post_mortem.py
+++ Post_mortem.py
@@ -69,16 +69,22 @@ def post_mortem(**kwargs):
         loggr.info('\tcolumn {} has an average of {} and an rmse of {}'. format(column, sum(dbp[column])/len(dbp), (sum(dbp[column].apply(lambda x: x**2))/len(dbp))**0.5))
 
     dbp_latest = dbp[dbp.date==fc_date]
-    loggr.info('For yesterday only:')
-    for column in ['ML_real_diff', 'ML_real_diff_r1', 'ML_real_diff_r0', 'mean_real_diff', 'TWN_real_diff', 'EC_real_diff', 'ave_real_diff']:
-        loggr.info('\tcolumn {} has an average of {} and an rmse of {}'. format(column, sum(dbp_latest[column])/len(dbp_latest), (sum(dbp_latest[column].apply(lambda x: x**2))/len(dbp_latest))**0.5))
+    dbp_latest.to_csv('/Users/Alex/Coding/weather_db/Data/prediction_db_analysis_latest.csv', index=False)
 
-    loggr.info("Total points to share: {}".format(len(dbp.ML_win)))
-    loggr.info("Total ML wins: {}".format(sum(dbp.ML_win)))
-    loggr.info("Total TWN wins: {}".format(sum(dbp.TWN_win)))
-    loggr.info("Total EC wins: {}".format(sum(dbp.EC_win)))
 
-    loggr.info("Latest points to share: {}".format(len(dbp_latest.ML_win)))
-    loggr.info("Latest ML wins: {}".format(sum(dbp_latest.ML_win)))
-    loggr.info("Latest TWN wins: {}".format(sum(dbp_latest.TWN_win)))
-    loggr.info("Latest EC wins: {}".format(sum(dbp_latest.EC_win)))
+    if len(dbp_latest) > 0:
+        loggr.info('For yesterday only:')
+        for column in ['ML_real_diff', 'ML_real_diff_r1', 'ML_real_diff_r0', 'mean_real_diff', 'TWN_real_diff', 'EC_real_diff', 'ave_real_diff']:
+            loggr.info('\tcolumn {} has an average of {} and an rmse of {}'. format(column, sum(dbp_latest[column])/len(dbp_latest), (sum(dbp_latest[column].apply(lambda x: x**2))/len(dbp_latest))**0.5))
+
+        loggr.info("Total points to share: {}".format(len(dbp.ML_win)))
+        loggr.info("Total ML wins: {}".format(sum(dbp.ML_win)))
+        loggr.info("Total TWN wins: {}".format(sum(dbp.TWN_win)))
+        loggr.info("Total EC wins: {}".format(sum(dbp.EC_win)))
+
+        loggr.info("Latest points to share: {}".format(len(dbp_latest.ML_win)))
+        loggr.info("Latest ML wins: {}".format(sum(dbp_latest.ML_win)))
+        loggr.info("Latest TWN wins: {}".format(sum(dbp_latest.TWN_win)))
+        loggr.info("Latest EC wins: {}".format(sum(dbp_latest.EC_win)))
+    else:
+        loggr.warning("No results from yesterday!")

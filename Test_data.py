2cae84d22f556d56705368646c1e35c4e9d1387d
Alex Roy <Alex@AlexMBP.local>
2018-12-08 21:19:52 -0500

    Fixed Issue where DataFrames were loaded once only
    Every time Daily.py calls one of these fucntions, Dataframes need to be reloaded. now they are!

--- Test_data.py
+++ Test_data.py
@@ -20,24 +20,20 @@ log_handler.setLevel(logging.INFO)
 loggr.addHandler(log_handler)
 loggr.setLevel(logging.INFO)
 
-
-fc, ht, rc = [
-    pd.read_csv("{}/Data/{}.csv".format(PATH, x), dtype={"date": "str"})
-    for x in ["forecast_db", "history_db", "region_codes"]
-]
-
-# create a copy of what was collected today
 today = str(datetime.datetime.now().date())
 yesterday = str(datetime.datetime.now().date() - datetime.timedelta(days=1))
 today = "{}-{}-{}".format(today[:4], today[5:7], today[8:10])
 yesterday = "{}-{}-{}".format(yesterday[:4], yesterday[5:7], yesterday[8:10])
-fc_today = fc[fc["date"] == today]
-ht_today = ht[ht["date"] == yesterday]
-region_count = rc["region"].count()
 
 
 # check for missed forecast data collection
 def check_forecast_data():
+    fc, rc = [
+        pd.read_csv("{}/Data/{}.csv".format(PATH, x), dtype={"date": "str"})
+        for x in ["forecast_db", "region_codes"]
+    ]
+    fc_today = fc[fc["date"] == today]
+    region_count = rc["region"].count()
     for provider in ["EC", "TWN"]:
         points_collected = fc_today[fc_today["provider"] == provider]["high"].count()
         if points_collected != 0:
@@ -55,6 +51,7 @@ def check_forecast_data():
 # This is to fix an EC bug where it seems like they record a high of '0' when
 # it should actually be NaN.
 def correct_bad_forecast_EC_data():
+    fc = pd.read_csv("{}/Data/forecast_db.csv".format(PATH), dtype={"date": "str"})
     drop_ind = list(
         fc[(fc.high == 0) & (fc.provider == "EC")].index
     )
@@ -66,6 +63,12 @@ def correct_bad_forecast_EC_data():
 
 # check for missed historical data collection
 def check_historical_data():
+    ht, rc = [
+        pd.read_csv("{}/Data/{}.csv".format(PATH, x), dtype={"date": "str"})
+        for x in ["history_db", "region_codes"]
+    ]
+    ht_today = ht[ht["date"] == yesterday]
+    region_count = rc["region"].count()
     for provider in ["EC", "TWN"]:
         points_collected = ht_today[ht_today["provider"] == provider]["high"].count()
         if points_collected != 0:
@@ -82,6 +85,7 @@ def check_historical_data():
 # This is to fix an EC bug where it seems like they record a high of '0' when
 # it should actually be NaN.
 def correct_bad_historical_EC_data():
+    ht = pd.read_csv("{}/Data/history_db.csv".format(PATH), dtype={"date": "str"})
     drop_ind = list(
         ht[(ht.high == 0) & (ht.provider == "EC")].index
     )
